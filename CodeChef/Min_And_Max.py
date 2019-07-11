def24ab08bfd90a6bad7bb9990bf6260dafbe29f
Siva Girish <sivagirish81@gmail.com>
2019-07-11 09:52:42 +0530

    CodeChef July 2019 Challenge

---
+++ CodeChef/Min_And_Max.py
@@ -0,0 +1,18 @@
+
+def MandM(N,K):
+    #Minimum possible is (n-1)/2
+    return K
+    
+
+
+
+
+        
+
+
+
+T=int(input())
+for i in range(T):
+    N=int(input())
+    K=int(input())
+    print(MandM(N,K))
