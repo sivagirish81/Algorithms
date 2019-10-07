2d0b27c5dbb9d317d2a5a7c846645b311bb6b144
Siva Girish <sivagirish81@gmail.com>
2019-10-07 20:31:34 +0530

    .

---
+++ Algo Notes/Graphs/Graph.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+class Graph
+{
+    int V;
+    
+}
--- Sorting/Merge_Sort.c
+++ Sorting/Merge_Sort.c
@@ -1,103 +1,106 @@
-/* C program for Merge Sort */
-#include<stdlib.h> 
-#include<stdio.h> 
+/*MergeSort(arr[], l,  r)
+If r > l
+     1. Find the middle point to divide the array into two halves:  
+             middle m = (l+r)/2
+     2. Call mergeSort for first half:   
+             Call mergeSort(arr, l, m)
+     3. Call mergeSort for second half:
+             Call mergeSort(arr, m+1, r)
+     4. Merge the two halves sorted in step 2 and 3:
+             Call merge(arr, l, m, r)
+*/
 
-// Merges two subarrays of arr[]. 
-// First subarray is arr[l..m] 
-// Second subarray is arr[m+1..r] 
-void merge(int arr[], int l, int m, int r) 
-{ 
-	int i, j, k; 
-	int n1 = m - l + 1; 
-	int n2 = r - m; 
 
-	/* create temp arrays */
-	int L[n1], R[n2]; 
+#include<stdio.h>
+#include<stdlib.h>
 
-	/* Copy data to temp arrays L[] and R[] */
-	for (i = 0; i < n1; i++) 
-		L[i] = arr[l + i]; 
-	for (j = 0; j < n2; j++) 
-		R[j] = arr[m + 1+ j]; 
+void input(int *x,int n);
+void mergesort(int *x,int l,int r);
+void merge(int *x,int l,int m,int r);
+void disp(int *x,int n);
 
-	/* Merge the temp arrays back into arr[l..r]*/
-	i = 0; // Initial index of first subarray 
-	j = 0; // Initial index of second subarray 
-	k = l; // Initial index of merged subarray 
-	while (i < n1 && j < n2) 
-	{ 
-		if (L[i] <= R[j]) 
-		{ 
-			arr[k] = L[i]; 
-			i++; 
-		} 
+int main()
+{
+	int n;
+	printf("Enter the no. of elements in the array:\n");
+	scanf("%d",&n);
+	printf("Enter the array to be sorted:\n");
+	int a[n];
+	input(a,n);
+	disp(a,n);
+	mergesort(a,0,n-1);
+	printf("The sorted array by merge sort is:\n");
+	disp(a,n);
+}
+
+void input(int *x,int n)
+{
+	printf("Enter elements into the array:\n");
+	for (int i=0;i<n;i++)
+	{
+		printf("Enter element no. %d:",(i+1));
+		scanf("%d",&x[i]);
+	}
+}
+
+void mergesort(int *x,int l,int r)
+{
+	if (l<r)
+	{
+		int m=l+(r-1)/2;
+		mergesort(x,l,m);
+		mergesort(x,m+1,r);
+		merge(x,l,m,r);
+	}
+}
+
+void merge(int *x,int l,int m,int r)
+{
+	int n1=m-l+1;
+	int a1[n1];			//Temporary storage of split arrays 1st half
+	int n2=r-m;
+	int a2[n2];		//Temporary storage of split arrays 2nd half
+	int i=0,j=0;
+	//Copying 1st half of array to temporary array
+	for (i=0;i<n1;i++)
+		a1[i]=x[l+i];
+	//copying 2nd half of array to temporary array
+	for (j=0;j<n2;j++)
+		a2[j]=x[m+1+j];
+	i=0;
+	j=0;
+	int k=l;
+	//Sorting the array
+	while (i<n1 && j<n2)
+	{
+		if (a1[i]<=a2[j])
+		{
+			x[k]=a1[i];
+			i++;
+		}
 		else
-		{ 
-			arr[k] = R[j]; 
-			j++; 
-		} 
-		k++; 
-	} 
-
-	/* Copy the remaining elements of L[], if there 
-	are any */
-	while (i < n1) 
-	{ 
-		arr[k] = L[i]; 
-		i++; 
-		k++; 
-	} 
-
-	/* Copy the remaining elements of R[], if there 
-	are any */
-	while (j < n2) 
-	{ 
-		arr[k] = R[j]; 
-		j++; 
-		k++; 
-	} 
-} 
-
-/* l is for left index and r is right index of the 
-sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r) 
-{ 
-	if (l < r) 
-	{ 
-		// Same as (l+r)/2, but avoids overflow for 
-		// large l and h 
-		int m = l+(r-l)/2; 
-
-		// Sort first and second halves 
-		mergeSort(arr, l, m); 
-		mergeSort(arr, m+1, r); 
-
-		merge(arr, l, m, r); 
-	} 
-} 
-
-/* UTILITY FUNCTIONS */
-/* Function to print an array */
-void printArray(int A[], int size) 
-{ 
-	int i; 
-	for (i=0; i < size; i++) 
-		printf("%d ", A[i]); 
-	printf("\n"); 
-} 
-
-/* Driver program to test above functions */
-int main() 
-{ 
-	int arr[] = {12, 11, 13, 5, 6, 7}; 
-	int arr_size = sizeof(arr)/sizeof(arr[0]); 
-
-	printf("Given array is \n"); 
-	printArray(arr, arr_size); 
-
-	mergeSort(arr, 0, arr_size - 1); 
-
-	printf("\nSorted array is \n"); 
-	printArray(arr, arr_size); 
-	return 0; 
-} 
+		{
+			x[k]=a2[j];
+			j++;
+		}
+		k++;
+	}
+	
+	//Copying the elements into the arraay x in sorted order from 1st half of array
+	while (i<n1)
+		x[k++]=a1[i++];
+	//Copying the elements into the arraay x in sorted order from 2nd half of array
+	while (j<n2)
+		x[k++]=a2[j++];
+}
+void disp(int *x,int n)
+{
+	for (int i=0;i<n;i++)
+	{
+		printf("%d\n",x[i]);
+	}
+}
+		
+		
+	
+	
