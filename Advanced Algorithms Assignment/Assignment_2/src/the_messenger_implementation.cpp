2721ec31dfb2437aaf62424837b7f24925137cd7
Siva Girish <sivagirish81@gmail.com>
2019-10-20 19:31:09 +0530

    .

--- Advanced Algorithms Assignment/Assignment_2/src/the_messenger_implementation.cpp
+++ Advanced Algorithms Assignment/Assignment_2/src/the_messenger_implementation.cpp
@@ -7,4 +7,21 @@ class the_messenger_implementation : public the_messenger
 {
 public:
     int solve(int, int, int, string);
+    //int Rec(int ,int , int , string );
 };
+
+int the_messenger_implementation::solve(int x,int y,int l,string m)
+{
+    int *DP=(int*)malloc(sizeof(int)*(l+1));
+    DP[0]=0;
+    DP[1]=x;
+    int res=0;
+    int temp=0;
+    for (int i=2;i<=l;i++)
+    {
+        for (int j=1;j<=i;j++)
+        {
+            
+        }
+    }
+}
