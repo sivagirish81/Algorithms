1ff29915d44c662ff426fe6830ea14320a66da7c
Siva Girish <sivagirish81@gmail.com>
2019-11-24 01:14:11 +0530

    Repository Cleaned

--- Advanced Algorithms Assignment/Assignment_2/src/the_messenger_implementation.cpp
+++ Advanced Algorithms Assignment/Assignment_2/src/the_messenger_implementation.cpp
@@ -19,6 +19,11 @@ int the_messenger_implementation::solve(int x,int y,int l,string s)
     int i = l-1;
     int j;
     int max;
+    // Maintains the state after each character has been inserted.
+    // When the first character is inserted there is no substring present aldready
+    // Therefore the only choice is to blindly insert the element so number of unique states is 0
+    // Future states there can be options either the a subset is aldready present or not.
+    // The number of states can be used to detemine how to pass the message
     while (i>=1)
     {
         max = 0;
@@ -27,11 +32,14 @@ int the_messenger_implementation::solve(int x,int y,int l,string s)
         {
             if (s[j] == s[i]) 
             {
+                // state j depends of the next  character to be inserted
                 state[j] = state[j+1] + 1;
                 if (j + state[j] - i> 0)      
                 {
                     state[j] = i - j;
                 }
+                // Finding the total states
+                // Max substring that is aldready present
                 if (state[j] > max)
                 {
                     max = state[j];
@@ -39,29 +47,41 @@ int the_messenger_implementation::solve(int x,int y,int l,string s)
             }
             else if (s[j] != s[i])
             {
+                // Currently state is set to 0
+                // As nothing matches at this point
+                // Possibly  beacause it is a new character (unique) or the first character to be inserted
                 state[j] = 0;
             }
             j+=1;
-        }  
+        }
+        // Assignig the state to max
+        // Max substring size will determine the total numer of states available
         state[i] = max;
         i-=1;
     }
     int maxim;
     int temp;
+    //initial cost is always x 
     DP[0]=x;
     i=1;
     j=0;
     while (i <l)
     {
+        // If the difference between inserting the new element and the cost for the insertion of previous charactrs i more tan x
+        // Add the new cost = x 
         if (DP[i] - DP[i-1] > x)
         {
             DP[i] = DP[i-1] + x;
         }
+        // Total number of ways to pass the message
         maxim = state[i];
+        // Passing substrings cost
         temp = DP[i-1] + y;
         j=0;
+        // For each size substring we calculate cost and store in DP
         while (j < maxim)
         {
+            // To get optimal minimal cost.
             if (temp < DP[i+j]) 
             {
                 DP[i+j] = temp;
@@ -76,6 +96,5 @@ int the_messenger_implementation::solve(int x,int y,int l,string s)
 int main()
 {
     the_messenger_implementation message;
-    //cout << message.solve(3,2,11,"AAAAABAAABA");
     cout << message.solve(3,1,4,"QQQQ");
 }
---
+++ Backup/survival_of_the_fittest_implementation.cpp
@@ -0,0 +1,39 @@
+#include "../include/survival_of_the_fittest.hpp"
+#include <vector>
+#include<iostream>
+using namespace std;
+
+
+class survival_of_the_fittest_implementation : public survival_of_the_fittest
+{
+public:
+    vector<vector<vector<vector<double>>>> DP =vector<vector<vector<vector<double>>>>(101,vector<vector<vector<double>>>(101,vector<vector<double>>(101,vector<double>(3))));
+    vector<double> solve(int, int, int);
+    vector<double> APK(int,int,int);
+};
+
+vector<double> survival_of_the_fittest_implementation::APK(int a,int p,int k)
+{
+    if(DP[a][p][k][0]!=0 && DP[a][p][k][1]!=0 && DP[a][p][k][2]!=0)
+        return DP[a][p][k];                 //If all the probabilities are zero that's the initial state.
+    if (a==0)                               //If the no. of archers is 0 then the pike men  will definitiely survive
+        return {0,1,0};             
+    if (p==0)
+        return {0,0,1};                     //If the no. of pikemen is 0 then the knights will definitiely survive
+    if (k==0)
+        return {1,0,0};                     //If the number of nights is 0 then the archers will definitely survive
+    //cout<<"HIHI";
+    vector<double> Aprob=APK(a,p-1,k);      //Probability that an archer kills a pikemen
+    vector<double> Pprob=APK(a,p,k-1);      //Probability that an pikemen kills a knight
+    vector<double> Kprob=APK(a-1,p,k);      //Probability that an knight kills an archer
+    DP[a][p][k][0]=( (a*p)*Aprob[0] + (p*k)*Pprob[0] +(k*a)*Kprob[0] ) / (a*p + p*k + k*a );    //Probability of archer surviving
+    DP[a][p][k][1]=( (a*p)*Aprob[1] + (p*k)*Pprob[1] +(k*a)*Kprob[1] ) / (a*p + p*k + k*a );    //Probability of pikemen surviving
+    DP[a][p][k][2]=( (a*p)*Aprob[2] + (p*k)*Pprob[2] +(k*a)*Kprob[2] ) / (a*p + p*k + k*a );    //Probability of knights surviving
+    return DP[a][p][k];         //Array containing the survival rate in the state where there are a archers,p pikemen and k knights alive.
+}
+
+vector<double> survival_of_the_fittest_implementation::solve(int a,int p,int k)
+{
+    vector<double> Prob=APK(a,p,k);
+    return Prob;
+}
